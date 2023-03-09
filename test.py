03ae0945735ef786c063a9f1dac863820c2636fe
GaspardSer <74904304+GaspardSer@users.noreply.github.com>
2023-03-09 16:20:30 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from gpiozero import LED
+from gpiozero import LED, Button
 """
 led1 = LED(4)
 led2 = LED(5)
@@ -12,3 +12,11 @@ for i in [4,5,6,13,19,26]:
     led.append(LED(i))
 
 led[0].on()
+
+button1 = Button(23)
+button2 = Button(24)
+while True:
+    if button1.is_pressed() == True:
+        led[0].on()
+    else:
+        led[0].off()
