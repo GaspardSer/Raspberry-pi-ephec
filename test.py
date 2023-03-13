983db96f6fd8c627e8a2ffed2d74333f7c9c898e
GaspardSer <gaspard.servais@gmail.com>
2023-03-13 23:31:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from gpiozero import LED
+from gpiozero import LED, Button
 import time
 
 led = [4, 5, 6, 13, 19, 26]
@@ -8,12 +8,16 @@ def onOff(led, t):
     led.on()
     time.sleep(t)
     led.off()
-    time.sleep(t)
 
 
-while True:
+#while True:
+if Button(23).is_pressed() == True:
     for i in led:
-        onOff(LED(i), 2)
+        onOff(LED(i), 0.1)
+if Button(24).is_pressed() == True:
+    for j in reversed(led):
+        onOff(LED(j), 0.1)
+
 
 """button1 = Button(23)
 button2 = Button(24)
