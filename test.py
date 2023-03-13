300e25459c809b23262e50cbf68460c7c33d82a4
GaspardSer <gaspard.servais@gmail.com>
2023-03-13 22:04:26 +0100

    Update test.py

--- test.py
+++ test.py
@@ -8,13 +8,13 @@ led4 = LED(13)
 led5 = LED(19)
 led6 = LED(26)
 """
-led = []
-for i in [4,5,6,13,19,26]:
-    led.append(LED(i))
+led = [LED(4), LED(5), LED(6), LED(13), LED(19), LED(26)]
+#for i in [4,5,6,13,19,26]:
+#    led.append(LED(i))
 
 while True:
     for i in led:
-        i.blink
+        i.blink()
         sleep(1)
 
 button1 = Button(23)
