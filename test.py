b3dfdb3c64dcf6e0cfc0ed31588d9cd35a11cae4
GaspardSer <gaspard.servais@gmail.com>
2023-03-13 22:14:48 +0100

    Update test.py

--- test.py
+++ test.py
@@ -8,19 +8,18 @@ led4 = LED(13)
 led5 = LED(19)
 led6 = LED(26)
 """
-led = [LED(4), LED(5), LED(6), LED(13), LED(19), LED(26)]
-#for i in [4,5,6,13,19,26]:
-#    led.append(LED(i))
+led = []
+for i in [4,5,6,13,19,26]:
+    led.append(i)
 
-while True:
-    for i in led:
-        i.blink()
-        sleep(1)
+for i in led:
+    LED(i).blink()
+    sleep(1)
 
-button1 = Button(23)
+"""button1 = Button(23)
 button2 = Button(24)
 while True:
     if button1.is_pressed() == True:
         led[0].on()
     else:
-        led[0].off()
+        led[0].off()"""
