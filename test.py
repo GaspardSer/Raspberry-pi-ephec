87c1e7a6e6bd0c078396c80899b51ffb24a2a965
GaspardSer <gaspard.servais@gmail.com>
2023-03-15 23:29:58 +0100

    Update test.py

--- test.py
+++ test.py
@@ -17,42 +17,40 @@ leds = [yellow_led1, yellow_led2, yellow_led3, yellow_led4]
 
 @app.route('/')
 def index():
-    # le 'f' devant nous permet de mettre des variables (en utilisant {}) dans le texte
-    return f"""
-
+    # note: les 3 guillemets permetent de définir une chaine de charactère multi-ligne
+    return """
 <!DOCTYPE html>
 <html>
 <head>
 <style>
 .button {
-    color: white;
-    padding: 16px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 50px;
-    transition-duration: 0.4s;
-    cursor: pointer;
-    color: black;
-    border: 2px solid #4CAF50;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 50px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  color: black;
+  border: 2px solid #4CAF50;
 }
 .button:hover {
-    background-color: #4CAF50;
-    color: white;
+  background-color: #4CAF50;
+  color: white;
 }
 </style>
 </head>
 <body>
-<h1>PLAY PING PONG </h1>
-<a href="/led" class="button">PLAY</a>
+<h1>Controle une LED</h1>
+<a href="/ping pong" class="button">Play</a>
 </body>
 </html>
 """
 
-app.run(host='0.0.0.0', port=8000, debug=True)
 
-@app.route('/led')
+@app.route('/pingpong')
 
 def blink_sequence(t):
     for i in range(len(leds)):
@@ -124,3 +122,5 @@ def play(t):
                 break
 
 play(0.5)
+
+app.run(host='0.0.0.0', port=8000, debug=True)
