cbafb13220a6f3f6c0ec5f47a210bd2f4a7a4874
GaspardSer <gaspard.servais@gmail.com>
2023-03-22 23:06:23 +0100

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -22,31 +22,82 @@ def index():
 <!doctype html>
 <html>
     <head>
-    <style>
-    .button {
-    color: white;
-    padding: 16px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 50px;
-    transition-duration: 0.4s;
-    cursor: pointer;
-    color: black;
-    border: 2px solid #4CAF50;
-    }
-    .button:hover {
-    background-color: #4CAF50;
-    color: white;
-    }
+        <style>
+
+        html {
+            background-color: rgb(16, 9, 44);
+        }
+        .button-64 {
+            align-items: center;
+            background-image: linear-gradient(144deg,#AF40FF, #5B42F3 50%,#00DDEB);
+            border: 0;
+            border-radius: 8px;
+            box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
+            box-sizing: border-box;
+            color: #FFFFFF;
+            display: flex;
+            font-family: Phantomsans, sans-serif;
+            font-size: 20px;
+            justify-content: center;
+            line-height: 1em;
+            max-width: 100%;
+            min-width: 140px;
+            padding: 3px;
+            text-decoration: none;
+            user-select: none;
+            -webkit-user-select: none;
+            touch-action: manipulation;
+            white-space: nowrap;
+            cursor: pointer;
+            }
+
+        .button-64:active, .button-64:hover {
+            outline: 0;
+        }
+
+        .button-64 span {
+            background-color: rgb(5, 6, 45);
+            padding: 16px 24px;
+            border-radius: 6px;
+            width: 100%;
+            height: 100%;
+            transition: 300ms;
+        }
+
+        .button-64:hover span {
+            background: none;
+        }
+
+        @media (min-width: 768px) {
+        .button-64 {
+                font-size: 100px;
+                min-width: 400px;
+            }
+        }
+
+        .center {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 200px;
+            }
+
+        h1{
+            text-align: center;
+            font-size: 100px;
+            color:  white;
+        }
     </style>
         <title>Ping Pong</title>
     </head>
     <body>
-        <h1>Press the button to play Ping Pong</h1>
+        <h1>Ping Pong</h1>
         <form action="/play" method="post">
-            <button class="button" type="submit">Play</button>
+            <div class="center">
+                <button class="button-64" role="button"><span class="text">Press to play</span></button>
+                    <!--Button from https://getcssscan.com/css-buttons-examples-->
+            </div>
+            
         </form>
     </body>
 </html>
